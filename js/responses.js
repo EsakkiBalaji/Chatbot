bfb61e887690877e2a9f8d18c05023ce8294a7c7
ESAKKI BALAJI M <esakkibalajim2001@gmail.com>
2024-01-29 02:44:49 -0800

    Add files via upload

---
+++ js/responses.js
@@ -0,0 +1,28 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello" || input == "hi" || input == "hey") {
+        return "Hello there!";
+    } else if (input == "goodbye" || input == "bye") {
+        return "Talk to you later!";
+    } else if (input == "what is your name" || input == "name") {
+        return "My Name is ChatBot!";
+    }
+    else if (input == "what is your age" || input == "age") {
+        return "My am 1 year old!";
+    }
+    else if (input == "what is your owner" || input == "owner") {
+        return "My Owner is Esakki Balaji!";
+    } else {
+        return "Try asking something else!";
+    }
+
+}
